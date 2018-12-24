f0bbbec756782f85fe3edb6ab6f3538b1b93f173
Azad Asanali <azadxperias2012@gmail.com>
2018-12-24 11:01:50 +0530

    Demo : Add rest service to spring mvc with content negotiating view resolver

--- src/main/java/com/neotechlabs/controller/MinutesController.java
+++ src/main/java/com/neotechlabs/controller/MinutesController.java
@@ -1,9 +1,15 @@
 package com.neotechlabs.controller;
 
+import com.neotechlabs.model.Activity;
 import com.neotechlabs.model.Exercise;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class MinutesController {
@@ -26,5 +32,24 @@ public class MinutesController {
 //        return "addMinutes";
 //
 //    }
+
+    @RequestMapping(value = "/activities", method = RequestMethod.GET)
+    public @ResponseBody List<Activity> findAllActivities() {
+        List<Activity> activities = new ArrayList<>();
+
+        Activity run = new Activity();
+        run.setDesc("Run");
+        activities.add(run);
+
+        Activity bike = new Activity();
+        bike.setDesc("Bike");
+        activities.add(bike);
+
+        Activity swim = new Activity();
+        swim.setDesc("Swim");
+        activities.add(swim);
+
+        return activities;
+    }
 
 }
